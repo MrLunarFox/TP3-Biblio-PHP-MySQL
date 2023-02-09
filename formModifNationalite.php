e72e1a429f7ced3ddc6e0aee0ff99b6f206d7e3a
HIS <hisjosuapro@gmail.com>
2023-02-09 12:08:22 +0100

    Portfolio V1.7

--- formModifNationalite.php
+++ formModifNationalite.php
@@ -1,19 +1,30 @@
 <?php include "header.php";
+include "connexionpdo.php";
+$num=$_GET['num'];
+$req=$monPdo->prepare("select * from nationalite where num= :num");
+$req->setFetchMode(PDO::FETCH_OBJ);
+$req->bindParam(':num', $num);
+$req->execute();
+$laNationalite=$req->fetch();
+var_dump($LaNationalite);
+
 ?>
 
     <div class="container mt-5">
 
-    <h2 class='pt-3 text-center'>Ajouter une nationalité</h2>
+    <h2 class='pt-3 text-center'>Modifier une nationalité</h2>
 
-        <form action="validAjoutNationalite.php" method="post" class="col-md-6 offset-md-3 border border-black p-3 rounded">
+        <form action="validModifNationalite.php" method="post" class="col-md-6 offset-md-3 border border-black p-3 rounded">
             <div class="form-group">
                 <label for="libelle"> Libellé </label>
-                <input type="text" class='form-control' id='libelle' placeholder='Saisir le libellé' name='libelle'>
+                <input type="text" class='form-control' id='libelle' placeholder='Saisir le libellé' name='libelle' value="<?php $laNationalite->libelle ?>">
             </div>
+
+            <input type="hidden" id="num" name="num" value="<?php $LaNationalite->num ?>">
 
             <div class="row">
                 <div class="col"><a href="listeNationalites.php" class='btn btn-warning btn-block'>Revenir à la liste</a></div>
-                <div class="col"><button type='submit' class='btn btn-success btn-block'>Ajouter</button></div>
+                <div class="col"><button type='submit' class='btn btn-success btn-block'>Modifier</button></div>
             </div>
 
         </form>
